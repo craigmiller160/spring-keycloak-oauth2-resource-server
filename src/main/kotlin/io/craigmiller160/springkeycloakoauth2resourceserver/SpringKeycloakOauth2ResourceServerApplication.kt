1d515052eecbfae32714a7adb5e7cd5ffdcf2f6d
Craig Miller <craigmiller160@gmail.com>
2023-06-03 10:48:27 -0400

    Formatted cod3

--- src/main/kotlin/io/craigmiller160/springkeycloakoauth2resourceserver/SpringKeycloakOauth2ResourceServerApplication.kt
+++ src/main/kotlin/io/craigmiller160/springkeycloakoauth2resourceserver/SpringKeycloakOauth2ResourceServerApplication.kt
@@ -3,9 +3,8 @@ package io.craigmiller160.springkeycloakoauth2resourceserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class SpringKeycloakOauth2ResourceServerApplication
+@SpringBootApplication class SpringKeycloakOauth2ResourceServerApplication
 
 fun main(args: Array<String>) {
-    runApplication<SpringKeycloakOauth2ResourceServerApplication>(*args)
+  runApplication<SpringKeycloakOauth2ResourceServerApplication>(*args)
 }
