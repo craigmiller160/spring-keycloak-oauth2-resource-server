a863b428d3c869a812f61ede7db64e0ebd52a499
Craig Miller <craigmiller160@gmail.com>
2023-06-03 17:28:43 -0400

    Made bean conditional

--- src/main/kotlin/io/craigmiller160/springkeycloakoauth2resourceserver/security/KeycloakOAuth2Customizers.kt
+++ src/main/kotlin/io/craigmiller160/springkeycloakoauth2resourceserver/security/KeycloakOAuth2Customizers.kt
@@ -1,12 +1,14 @@
 package io.craigmiller160.springkeycloakoauth2resourceserver.security
 
 import io.craigmiller160.springkeycloakoauth2resourceserver.config.KeycloakResourceServerConfig
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 
 @Component
 class KeycloakOAuth2Customizers(private val config: KeycloakResourceServerConfig) {
@@ -17,6 +19,7 @@ class KeycloakOAuth2Customizers(private val config: KeycloakResourceServerConfig
         spec.jwt { jwt -> jwt.jwtAuthenticationConverter(WebFluxJwtAuthConverter(config)) }
       }
 
+  @ConditionalOnClass(Mono::class)
   @Bean("webmvcKeycloakOAuth2ResourceServer")
   fun webmvcKeycloakOAuth2ResourceServer():
       Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>> =
