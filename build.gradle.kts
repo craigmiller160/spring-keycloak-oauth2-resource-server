6756f5182f4e3b108b494ad5ee5c5533d0fdd10f
Craig Miller <craigmiller160@gmail.com>
2023-06-03 10:48:16 -0400

    More project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val projectGroup: String by project
+val projectVersion: String by project
 
 plugins {
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
+    id("io.craigmiller160.gradle.defaults") version "1.1.0"
+    id("com.diffplug.spotless") version "6.17.0"
+    kotlin("jvm")
+    kotlin("plugin.spring")
 }
 
-group = "io.craigmiller160"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
-repositories {
-    mavenCentral()
-}
+group = projectGroup
+version = projectVersion
+java.sourceCompatibility = JavaVersion.VERSION_19
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
@@ -26,10 +28,16 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "19"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+configure<SpotlessExtension> {
+    kotlin {
+        ktfmt()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
-rootProject.name = "spring-keycloak-oauth2-resource-server"
+val projectName: String by settings
+
+rootProject.name = projectName
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+    }
+}
